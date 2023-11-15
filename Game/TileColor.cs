c3f10536d55b43ab5c96c8e13a017c73f3541b1d
William Liew <william.liew@qesnet.com>
2023-11-15 10:11:25 +0800

    Update TileColors to Material Design palette 500

--- Game/TileColor.cs
+++ Game/TileColor.cs
@@ -13,11 +13,11 @@ public class TileColor
 
         Colors[(int)TetrominoShapes.None] = new Color(0x30 / 255f, 0x30 / 255f, 0x30 / 255f, 1);
         Colors[(int)TetrominoShapes.I] = new Color(0x00 / 255f, 0xBC / 255f, 0xD4 / 255f, 1);
-        Colors[(int)TetrominoShapes.J] = new Color(0x29 / 255f, 0x62 / 255f, 0xFF / 255f, 1);
-        Colors[(int)TetrominoShapes.L] = new Color(0xFF / 255f, 0x6D / 255f, 0x00 / 255f, 1);
-        Colors[(int)TetrominoShapes.O] = new Color(0xFF / 255f, 0xD6 / 255f, 0x00 / 255f, 1);
-        Colors[(int)TetrominoShapes.S] = new Color(0x00 / 255f, 0xC8 / 255f, 0x53 / 255f, 1);
-        Colors[(int)TetrominoShapes.T] = new Color(0xAA / 255f, 0x00 / 255f, 0xFF / 255f, 1);
+        Colors[(int)TetrominoShapes.J] = new Color(0x21 / 255f, 0x96 / 255f, 0xF3 / 255f, 1);
+        Colors[(int)TetrominoShapes.L] = new Color(0xFF / 255f, 0x98 / 255f, 0x00 / 255f, 1);
+        Colors[(int)TetrominoShapes.O] = new Color(0xFF / 255f, 0xEB / 255f, 0x3B / 255f, 1);
+        Colors[(int)TetrominoShapes.S] = new Color(0x4C / 255f, 0xAF / 255f, 0x50 / 255f, 1);
+        Colors[(int)TetrominoShapes.T] = new Color(0x9C / 255f, 0x27 / 255f, 0xB0 / 255f, 1);
         Colors[(int)TetrominoShapes.Z] = new Color(0xF4 / 255f, 0x43 / 255f, 0x36 / 255f, 1);
     }
 
